b099bde678871c67e52cd7db7c94e5a527322245
ChromeGG <imie.nazwisko69.at@gmail.com>
2019-10-28 17:08:20 +0100

    Initial commit

---
+++ src/main/java/com/example/compario/ComparioApplication.java
@@ -0,0 +1,13 @@
+package com.example.compario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComparioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/compario/ComparioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
