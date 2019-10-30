a7ad253abeac9ee168286cbd21653f675254efaa
ChromeGG <imie.nazwisko69.at@gmail.com>
2019-10-30 21:48:50 +0100

    Optimize imports in CurrencyValue

--- src/main/java/com/example/compario/model/CurrencyValue.java
+++ src/main/java/com/example/compario/model/CurrencyValue.java
@@ -1,13 +1,9 @@
 package com.example.compario.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.math.BigDecimal;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
